bd1abfdce05fec5ed8490c05ecce8386ecc8595e
alssaathexplore <zalsamutiara@gmail.com>
2025-11-10 17:57:39 +0700

    Refactor produk.php for improved readability

--- coba_iva/produk.php
+++ coba_iva/produk.php
@@ -1,24 +1,38 @@
 <?php
-include 'koneksi.php';
+// Menghubungkan ke database
+require 'koneksi.php';
+
+// Menangkap ID kategori dari URL (?id=...)
 $id = $_GET['id'];
-$kategori = mysqli_query($conn, "SELECT * FROM categories WHERE CategoryID='$id'");
+
+// Mengambil nama kategori sesuai ID yang dipilih
+$kategori = mysqli_query($koneksi, "SELECT * FROM categories WHERE CategoryID='$id'");
 $data_kat = mysqli_fetch_array($kategori);
 ?>
+
 <html>
-<head><title><u>PRODUK <?php echo $data_kat['CategoryName']; ?></title></head></u>
+<head><title><u>PRODUK <?php echo $data_kat['CategoryName']; ?></u></title></head>
 <body>
-<h2 align="center"><u>Daftar Produk : <i> <?php echo $data_kat['CategoryName']; ?></h2> </i></u>
+<h2 align="center"><u>Daftar Produk : <i><?php echo $data_kat['CategoryName']; ?></i></u></h2>
+
+<!-- Membuat tabel untuk daftar produk -->
 <table border="1" align="center" cellpadding="5" cellspacing="0">
 <tr bgcolor="#e6f2ff">
     <th>ID Produk</th>
     <th>Nama Produk</th>
     <th>Harga</th>
 </tr>
+
 <?php
-$no = 1;
-$query = mysqli_query($conn, "SELECT * FROM products WHERE CategoryID='$id'");
+$no = 1; // Nomor baris
+// Menampilkan produk berdasarkan kategori yang dipilih
+$query = mysqli_query($koneksi, "SELECT * FROM products WHERE CategoryID='$id'");
+
 while ($data = mysqli_fetch_array($query)) {
+    // Pewarnaan baris: biru muda dan pink muda bergantian
     $warna = ($no % 2 == 0) ? "#e6f2ff" : "#ffe6f2";
+
+    // Menampilkan data produk, nama produk dapat diklik menuju halaman detail
     echo "<tr bgcolor='$warna'>
             <td>{$data['ProductID']}</td>
             <td><a href='detail.php?id={$data['ProductID']}'>{$data['ProductName']}</a></td>
@@ -28,6 +42,8 @@ while ($data = mysqli_fetch_array($query)) {
 }
 ?>
 </table>
+
+<!-- Tombol kembali ke halaman kategori -->
 <p align="center"><a href="index.php">Kembali ke Kategori</a></p>
 </body>
 </html>
